93bfa26a33a3f50043d452a21cabc3b72d2c3f13
diyorbekjuraev-nordra <diyorbek.juraev@nordra.com>
2026-01-26 19:39:28 +0500

    first commit

--- apps/typix/mdx-components.tsx
+++ apps/typix/mdx-components.tsx
@@ -2,6 +2,7 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  // @ts-ignore
   return {
     ...defaultMdxComponents,
     ...components,
