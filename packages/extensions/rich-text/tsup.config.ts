317bc6fc4546bda5876697500b28c0e139c99c04
diyorbekjuraev-nordra <diyorbek.juraev@nordra.com>
2026-01-26 18:08:46 +0500

    first commit

--- packages/extensions/rich-text/tsup.config.ts
+++ packages/extensions/rich-text/tsup.config.ts
@@ -1,22 +1,17 @@
-import { defineConfig, type Options } from 'tsup';
+import { defineConfig } from 'tsup';
 
-export function createExtensionConfig(options?: Options) {
-    return defineConfig({
-        format: ['cjs', 'esm'],
-        dts: true,
-        splitting: false,
-        sourcemap: true,
-        clean: true,
-        treeshake: true,
-        target: 'es2022',
-        platform: 'browser',
-        external: [
-            'react',
-            'react-dom',
-            'lexical',
-            '@lexical/*',
-            '@typix/*'
-        ],
-        ...options,
-    });
-}
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    target: 'es2022',
+    external: [
+        'react',
+        'react-dom',
+        'lexical',
+        /@lexical\/.*/,
+        '@typix/core',
+    ],
+});
--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,22 +1,18 @@
-import { defineConfig, type Options } from 'tsup';
 
-export function createExtensionConfig(options?: Options) {
-    return defineConfig({
-        format: ['cjs', 'esm'],
-        dts: true,
-        splitting: false,
-        sourcemap: true,
-        clean: true,
-        treeshake: true,
-        target: 'es2022', 
-        platform: 'browser',
-        external: [
-            'react',
-            'react-dom',
-            'lexical',
-            '@lexical/*',
-            '@typix/*'
-        ],
-        ...options,
-    });
-}
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    target: 'es2022',
+    external: [
+        'react',
+        'react-dom',
+        'lexical',
+        /@lexical\/.*/,
+        '@typix/core',
+    ],
+});
