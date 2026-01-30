ecf04b47a167878e7da27fb457cb9007d86571ed
diyorbekjuraev-nordra <diyorbek.juraev@nordra.com>
2026-01-31 00:23:00 +0500

    feat(link-and-collapsible): add flexible configuration and validation

---
+++ packages/extensions/collapsible/src/index.ts
@@ -0,0 +1,5 @@
+export { CollapsiblePlugin, INSERT_COLLAPSIBLE_COMMAND } from './extension';
+export { $convertDetailsElement, $createCollapsibleContainerNode, $isCollapsibleContainerNode, CollapsibleContainerNode } from "./nodes/container";
+export { $createCollapsibleContentNode, $isCollapsibleContentNode, CollapsibleContentNode } from "./nodes/content";
+export { $createCollapsibleTitleNode, $isCollapsibleTitleNode, CollapsibleTitleNode } from "./nodes/title"; 
+ 
